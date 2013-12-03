a904124a071dcc592efc3fe14b993bd254b307f7
Hannah Siegel <hannah.k.siegel@gmail.com>
2013-12-03 13:15:01 +0100

    made command line input

---
+++ PIBerechnung/src/Rmi1/Server.java
@@ -0,0 +1,7 @@
+package Rmi1;
+
+public class Server {
+	public Server(String ip){
+		
+	}
+}
--- PIBerechnung/src/others/TestKlasse.java
+++ PIBerechnung/src/others/TestKlasse.java
@@ -1,7 +1,41 @@
 package others;
 
+import Rmi1.Client;
+import Rmi1.Server;
+
 public class TestKlasse {
-	public static void main (String arg[]){
-		
+	public static void main(String arg[]) {
+		if (Methods.checkArguments(arg)) {
+			if (arg[0].equals("Client")) {
+				String ip = "";
+				int number = 0;
+				for (int i = 0; i < arg.length; i++) {
+					if (arg[i].equalsIgnoreCase("-a")) {
+						ip = arg[i + 1];
+					}
+					if (arg[i].equalsIgnoreCase("-n")) {
+						number = Integer.parseInt(arg[i + 1]);
+
+					}
+				}
+				new Client(ip, number);
+			}
+
+			if (arg[0].equals("Balancer")) {
+
+				// new Balancer();
+			}
+			if (arg[0].equals("Server")) {
+				String ip = "";
+				for (int i = 0; i < arg.length; i++) {
+					if (arg[i].equalsIgnoreCase("-a")) {
+						ip = arg[i + 1];
+					}
+				}
+				new Server(ip);
+			}
+		} else {
+			Methods.usage();
+		}
 	}
 }
