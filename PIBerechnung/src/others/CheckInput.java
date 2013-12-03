7d827c4e4887b83ff22cd39595ea567cfbfd9827
Hannah Siegel <hannah.k.siegel@gmail.com>
2013-12-03 12:37:38 +0100

    Beinhaltet Aufwandschaetzung und allgemeine Docs.

---
+++ PIBerechnung/src/others/CheckInput.java
@@ -0,0 +1,12 @@
+package others;
+
+public class CheckInput {
+	public static boolean checkArgs(String arg[]){
+		
+		return true;
+	}
+
+	public static boolean checkArguments(String arg[]) {
+		if(arg.length<3)
+	}
+}
